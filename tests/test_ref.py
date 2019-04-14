09e68fbe2b1f76504203d85df1ebb7dc2cc30ca6
Steven Cole <scole66@gmail.com>
2019-04-13 17:46:00 -0700

    Add tests for `GetValue`

--- tests/test_ref.py
+++ tests/test_ref.py
@@ -1,6 +1,10 @@
 import pytest
+import math
 
 from ecmascript import *
+
+THROW = CompletionType.THROW
+NORMAL = CompletionType.NORMAL
 
 @pytest.fixture
 def realm():
@@ -65,3 +69,41 @@ def test_IsSuperReference_01(obj, hasSuper, expected):
     if hasSuper:
         ref.this_value = obj
     assert IsSuperReference(ref) == expected
+
+@pytest.mark.parametrize('ref, expected', [
+    (Completion(THROW, 'thrown', None), Completion(THROW, 'thrown', None)),  # Input is abrupt completion
+    ('monkey', Completion(NORMAL, 'monkey', None)), # Input is not a reference
+])
+def test_GetValue_01(ref, expected):
+    rval = GetValue(ref)
+    assert rval == expected
+
+def test_GetValue_02():
+    # If the input reference is unresolvable, throw a reference error.
+    rval = GetValue(Reference(None, 'unresolvable', False))
+    assert rval.ctype == THROW
+    assert isinstance(rval.value, ReferenceError)
+    assert rval.target is None
+
+def test_GetValue_03(obj):
+    # The common case: resolve the reference to an object
+    CreateDataProperty(obj, 'value', 'elephant')
+    rval = GetValue(Reference(obj, 'value', False))
+    assert rval == Completion(NORMAL, 'elephant', None)
+
+@pytest.mark.xfail(reason='Needs String Object support')
+def test_GetValue_04():
+    # Now, the case where the base is a common object, and we try and get a property from that. One example is the 'length'
+    # property of strings. This will fail for now, because I don't have string objects implemented.
+    rval = GetValue(Reference('short', 'length', False))
+    assert rval == Completion(NORMAL, 5, None)
+
+def test_GetValue_05():
+    # But we can still try to dereference a value from a primitive type object; we should just get None back.
+    rval = GetValue(Reference(45, 'digits', False))
+    assert rval == Completion(NORMAL, None, None)
+
+def test_GetValue_06(realm):
+    # Finally, there's the Environment Record form.
+    rval = GetValue(Reference(realm.global_env.environment_record, 'NaN', False))
+    assert rval == Completion(NORMAL, math.nan, None)
