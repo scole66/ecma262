7acbd53be6895ac2b9f4aa4b26f1a3644580470b
Steven Cole <scole66@gmail.com>
2019-06-09 17:12:02 -0700

    Add lexer handling for regular expressions and templates
    And tests!

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -1,7 +1,12 @@
 import pytest
+from itertools import chain
 
 from ecmascript import *
 
+InputElementDiv = Lexer.Goal.InputElementDiv
+InputElementRegExp = Lexer.Goal.InputElementRegExp
+InputElementRegExpOrTemplateTail = Lexer.Goal.InputElementRegExpOrTemplateTail
+InputElementTemplateTail = Lexer.Goal.InputElementTemplateTail
 
 def test_lex_simple_sample():
     input_text = "goat = pig + beard - curly tail"
@@ -35,6 +40,8 @@ def test_lex_simple_sample():
                              ('&&', [{'type': 'AMPAMP', 'value': '&&'}]),
                              ('&=', [{'type': 'AMPEQ', 'value': '&='}]),
                              ('(', [{'type': 'LPAREN_', 'value': '('}]),
+                             ('(let', [{'type': 'LPAREN_LET', 'value': '('}, {'type': 'LET', 'value': 'let'}]),
+                             ('([', [{'type': 'LPAREN_LBRACKET', 'value': '('}, {'type': 'LBRACKET', 'value': '['}]),
                              (')', [{'type': 'RPAREN', 'value': ')'}]),
                              ('*', [{'type': 'STAR', 'value': '*'}]),
                              ('**', [{'type': 'STARSTAR', 'value': '**'}]),
@@ -89,7 +96,7 @@ def test_lex_simple_sample():
                              ('\r\n\u2028\u2029', []),
                             ('!// This is a comment', [{'type': 'BANG', 'value': '!'}]),
                             ('''%/* comment that
-can even be
+can even\r\nbe
 multiple lines!
 */''', [{'type': 'PERCENT', 'value': '%'}]),
                             ('//comment\n', []),
@@ -178,6 +185,31 @@ multiple lines!
                                 {'type': 'IDENTIFIER', 'value': 'NaN'},
                                 {'type': 'SEMICOLON', 'value': ';'},
                                 ]),
+                            ('`no_subs`', [{'type': 'NOSUBSTITUTIONTEMPLATE', 'value': ('no_subs', 'no_subs')}]),
+                            ('``', [{'type': 'NOSUBSTITUTIONTEMPLATE', 'value': ('', '')}]),
+                            ('`${', [{'type': 'TEMPLATEHEAD', 'value': ('', '')}]),
+                            ('`abcd\\${}..${', [{'type': 'TEMPLATEHEAD', 'value': ('abcd${}..', 'abcd\\${}..')}]),
+                            ('`ab\\xty${', [{'type': 'TEMPLATEHEAD', 'value': (None, 'ab\\xty')}]),
+                            (r'`ab\x6ty${', [{'type': 'TEMPLATEHEAD', 'value': (None, r'ab\x6ty')}]),
+                            (r'`ab\u{1234}cd${', [{'type': 'TEMPLATEHEAD', 'value': ('ab\u1234cd', r'ab\u{1234}cd')}]),
+                            (r'`ab\u{12349aabcdde3}cd${', [{'type': 'TEMPLATEHEAD', 'value': (None, r'ab\u{12349aabcdde3}cd')}]),
+                            (r'`ab\u000a\u000d`', [{'type': 'NOSUBSTITUTIONTEMPLATE', 'value': ('ab\n\r', r'ab\u000a\u000d')}]),
+                            (r'`ab\u45tonya`', [{'type': 'NOSUBSTITUTIONTEMPLATE', 'value': (None, r'ab\u45tonya')}]),
+                            (r'`ab\u1zball`', [{'type': 'NOSUBSTITUTIONTEMPLATE', 'value': (None, r'ab\u1zball')}]),
+                            (r'`ab\xball`', [{'type': 'NOSUBSTITUTIONTEMPLATE', 'value': ('ab\xball', r'ab\xball')}]),
+                            (r'`ab\09ty`', [{'type': 'NOSUBSTITUTIONTEMPLATE', 'value': (None, r'ab\09ty')}]),
+                            (r'`ab\0xy`', [{'type': 'NOSUBSTITUTIONTEMPLATE', 'value': ('ab\0xy', r'ab\0xy')}]),
+                            ('`ab\\\r\ncd`', [{'type': 'NOSUBSTITUTIONTEMPLATE', 'value': ('abcd', 'ab\\\ncd')}]),
+                            ('`ab\\\rcd`', [{'type': 'NOSUBSTITUTIONTEMPLATE', 'value': ('abcd', 'ab\\\ncd')}]),
+                            ('`ab\\\u2028cd\\\u2029ef\\\ngh`', [{'type': 'NOSUBSTITUTIONTEMPLATE', 'value': ('abcdefgh', 'ab\\\u2028cd\\\u2029ef\\\ngh')}]),
+                            ('`ab\ncd\u2028ef\u2029gh\rij\r\nkl`', [{'type': 'NOSUBSTITUTIONTEMPLATE', 'value': ('ab\ncd\u2028ef\u2029gh\nij\nkl', 'ab\ncd\u2028ef\u2029gh\nij\nkl')}]),
+                            ('`ab\\xpop\n`', [{'type': 'NOSUBSTITUTIONTEMPLATE', 'value': (None, 'ab\\xpop\n')}]),
+                            ('`ab\\xpop\r`', [{'type': 'NOSUBSTITUTIONTEMPLATE', 'value': (None, 'ab\\xpop\n')}]),
+                            ('`ab\\xpop\\0poop`', [{'type': 'NOSUBSTITUTIONTEMPLATE', 'value': (None, 'ab\\xpop\\0poop')}]),
+                            ('`a\\xp\\n`', [{'type': 'NOSUBSTITUTIONTEMPLATE', 'value': (None, 'a\\xp\\n')}]),
+                            ('`a\\xp\\x3b`', [{'type': 'NOSUBSTITUTIONTEMPLATE', 'value': (None, 'a\\xp\\x3b')}]),
+                            ('`a\\xp\\u003b`', [{'type': 'NOSUBSTITUTIONTEMPLATE', 'value': (None, 'a\\xp\\u003b')}]),
+                            ('`a\\xp\\u{3b}`', [{'type': 'NOSUBSTITUTIONTEMPLATE', 'value': (None, 'a\\xp\\u{3b}')}]),
              ])
 def test_lex(test_input, expected):
     l = Lexer(test_input)
@@ -188,14 +220,62 @@ def test_lex(test_input, expected):
                          [ '/* unterminated', '0000', '0b1015', '0o3129', '0x', '0b', '0o', '3e', '0xab$', '0b0100r', '0o765_', '67.22e+21\u2118', '432A', '7.Z',
                            'id_\\tab', '\\tab', '_\\utab', '\\utab', '_\\u0m', '\\u0m',
                            'id_\\u005cu0040',
-                           '_\\u00m', '\\u00m', '_\\u000m', '\\u000m', '_\\u{718Q', '\\u{718Q',
+                           '_\\u00m', '\\u00m', '_\\u000m', '\\u000m', '_\\u{718Q', '\\u{718Q', '\\u{3a}bob', '\\u003abob', 'a\\u{3a}bob', 'a\\u003abob',
+                           '\\u{47381907950801}',
                            '_\\u{738190789503417839105170}', '\\u{}p', '_\\u{}p', 'Embedded\\u0000Null',
                            "'\\", "'", "'ab\ncd'", "'a\\09b'", "'ab\\xAX'", "'ab\\uX0000zzz'",
-                           "'ab\\u67XX'", "'ab\\u{}XX'", "'ab\\u{789401578910}'", "'ab\\u{###}'"])
+                           "'ab\\u67XX'", "'ab\\u{}XX'", "'ab\\u{789401578910}'", "'ab\\u{###}'",
+                           r'`ab\u45', '`ab\\', '\0',
+                                   ])
 def test_lex_syntax_error(test_input):
     l = Lexer(test_input)
     with pytest.raises(LexerError):
         result = list(l.lex())
+
+@pytest.mark.parametrize('test_input, test_goals, expected', [
+    ('/abcd/', [InputElementRegExp], [{'type': 'REGEXP', 'value': ('abcd', '')}]),
+    ('/ab[0-9]cd/g', [InputElementRegExp], [{'type': 'REGEXP', 'value': ('ab[0-9]cd', 'g')}]),
+    ('/ab\\ncd/g', [InputElementRegExp], [{'type': 'REGEXP', 'value': ('ab\\ncd', 'g')}]),
+    (r'/ab[\t\r\n]/g', [InputElementRegExp], [{'type': 'REGEXP', 'value': (r'ab[\t\r\n]', 'g')}]),
+    (r'/ab/\u0061\u0069', [InputElementRegExp], [{'type': 'REGEXP', 'value': ('ab', r'\u0061\u0069')}]),
+    (r'/ab/\u{1234}', [InputElementRegExp], [{'type': 'REGEXP', 'value': ('ab', r'\u{1234}')}]),
+    ('// This is a comment //', [InputElementRegExp], [None]),
+    ('/* comment */ blue', [InputElementRegExp], [{'type': 'IDENTIFIER', 'value': 'blue'}]),
+    ('/\\t/', [InputElementRegExp], [{'type': 'REGEXP', 'value': ('\\t', '')}]),
+    ('/[0-9]/', [InputElementRegExp], [{'type': 'REGEXP', 'value': ('[0-9]', '')}]),
+    ('}${', [InputElementTemplateTail], [{'type': 'TEMPLATEMIDDLE', 'value': ('', '')}]),
+    ('}`', [InputElementTemplateTail], [{'type': 'TEMPLATETAIL', 'value': ('', '')}]),
+    ('}bob${', [InputElementTemplateTail], [{'type': 'TEMPLATEMIDDLE', 'value': ('bob', 'bob')}]),
+])
+def test_lex_goalchange(test_input, test_goals, expected):
+    l = Lexer(test_input)
+    result = [l.lex_next_token(goal) for goal in chain((InputElementDiv,), test_goals)][1:]
+    assert [{'type': token.type, 'value': token.value.value} if token else token for token in result] == expected
+
+@pytest.mark.parametrize('test_input, test_goals', [
+    ('/abcd\n', [InputElementRegExp]),
+    ('/ab\\', [InputElementRegExp]),
+    ('/ab[', [InputElementRegExp]),
+    ('/ab[9\\', [InputElementRegExp]),
+    (r'/ab/\d', [InputElementRegExp]),
+    (r'/ab/\u{6', [InputElementRegExp]),
+    (r'/ab/\up', [InputElementRegExp]),
+    (r'/ab/\u1p', [InputElementRegExp]),
+    (r'/ab/\u12p', [InputElementRegExp]),
+    (r'/ab/\u123p', [InputElementRegExp]),
+    (r'/ab/\u{123p', [InputElementRegExp]),
+    (r'/ab/\u{}p', [InputElementRegExp]),
+    ('/', [InputElementRegExp]),
+    ('/ab/\\u003ag', [InputElementRegExp]),
+    ('/ab/\\u{987654321}', [InputElementRegExp]),
+    ('/ab/\\u{3a}', [InputElementRegExp]),
+    ('}', [InputElementTemplateTail]),
+])
+def test_lex_goalchange_errs(test_input, test_goals):
+    l = Lexer(test_input)
+    with pytest.raises(LexerError):
+        result = [l.lex_next_token(goal) for goal in chain((InputElementDiv,), test_goals)]
+
 
 def test_lex_tokenvalue_repr():
     tv = Lexer.TokenValue()
