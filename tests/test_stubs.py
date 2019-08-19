b8d51a12cb1557687caee88c07d2bb701255e41b
Steven Cole <scole66@gmail.com>
2019-08-19 08:23:33 -0700

    Add a stub test

---
+++ tests/test_stubs.py
@@ -0,0 +1,12 @@
+# Test-Stubs
+#
+# This is an in-progress test file, ensuring that our stubs do stub-like things. It's here to help get to complete
+# test coverage. When the project is complete and there are no more stubs, this file should be empty.
+
+import pytest
+
+import ecmascript
+
+def test_Await():
+    with pytest.raises(NotImplementedError):
+        ecmascript.Await('arg')
